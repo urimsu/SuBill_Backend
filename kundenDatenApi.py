783a7184b20647f8c060ebdec462a0afa378ded2
urimsu <urimsu56@gmail.com>
2025-05-09 00:38:40 +0200

    smallfix

--- kundenDatenApi.py
+++ kundenDatenApi.py
@@ -6,10 +6,10 @@ import os
 
 originLink="https://subill.su-tech.org"
 
-host_name=os.environ.get(hostname)
-host_user=os.environ.get(hostuser)
-host_password=os.environ.get(hostpassword)
-host_database=os.environ.get(hostdatabase)
+host_name=os.environ.get(hostname)# type: ignore
+host_user=os.environ.get(hostuser)# type: ignore
+host_password=os.environ.get(hostpassword)# type: ignore
+host_database=os.environ.get(hostdatabase) # type: ignore
 
 
 app = Flask(__name__)
